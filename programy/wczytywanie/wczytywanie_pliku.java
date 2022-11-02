e590a9794ec87ef1ae8bf61d92fff3b57f3739c5
Abaddon7063554 <109863155+Abaddon7063554@users.noreply.github.com>
2022-11-02 18:21:05 +0100

    Abaddon24

--- programy/wczytywanie/wczytywanie_pliku.java
+++ programy/wczytywanie/wczytywanie_pliku.java
@@ -7,12 +7,19 @@ public class wczytywanie_pliku {
     public static void main(String[] args) throws FileNotFoundException {
 
         Scanner pobierz = new Scanner(new File("C:\\Users\\Łukasz\\Documents\\GitHub\\Java\\programy\\wczytywanie\\plik.txt"));
+        double srednia=0,i=1;
+        while(pobierz.hasNextDouble()) //zwraca true jezeli następna wartość z pliku
+        {                            // jest interpretowana jako int.
+           srednia +=pobierz.nextDouble();
+           i++;
 
-        while(pobierz.hasNextInt()) //zwraca true jezeli następna wartość z pliku
-        {                            //daje jest interpretowana jako int.
-            System.out.println(pobierz.nextInt());
-        //    System.out.println(pobierz.skip("1"));
         }
+        System.out.println(i);
+        System.out.println(srednia);
+        System.out.println(srednia/i);
+
+
+
         pobierz.close();
     }
 }
