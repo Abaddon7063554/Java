980bfd72cb03a29441359d4a80de2e5829ce0055
Abaddon7063554 <109863155+Abaddon7063554@users.noreply.github.com>
2022-11-06 01:40:53 +0100

    Abaddon24

---
+++ programy/StringBuffer_and_StringBuilder/stringi.java
@@ -0,0 +1,7 @@
+public class stringi {
+    public static void main(String[] args) {
+        StringBuffer s = new StringBuffer("NAPIS");
+        System.out.println(s.indexOf("N"));
+        System.out.println(s.appendCodePoint(23));
+    }
+}
